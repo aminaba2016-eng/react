b244ca19f21bf6ba865b2136b0e76a77c860a548
Amina <amina.bahry5@gmail.com>
2021-05-05 13:07:46 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import CheckForm from './component/CheckForm.js';
+
+function App() {
+  return (
+    <div className="App">
+      <CheckForm/>
+      </div>
+      
+    
+  );
+}
+
+export default App;
---
+++ src/component/CheckForm.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+ 
+
+function CheckForm() {
+    return (
+        <div className=''>
+<div className="form-group">
+    <label for="email">Email address:</label>
+    <input type="email" class="form-control" id="email"/>
+  </div>
+
+        </div>
+        
+    )
+};
+
+export default CheckForm;
